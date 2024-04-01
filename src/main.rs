006b277a4f4cc980e66b76497f02025b2c0b90df
MT <04x0xx@gmail.com>
2024-04-01 02:26:34 -0300

    Got computation working with GPU

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,232 @@
 extern crate vulkano;
 extern crate winit;
 
-use std::sync::Arc;
-
-use winit::event_loop::{EventLoop, EventLoopBuilder};
-use winit::window::WindowBuilder;
-
 use vulkano::{
+    buffer::{
+        Buffer,
+        BufferCreateInfo,
+        BufferUsage,
+    },
+    pipeline::{
+        Pipeline,
+        PipelineShaderStageCreateInfo,
+        PipelineBindPoint,
+        ComputePipeline,
+        PipelineLayout,
+        layout::PipelineDescriptorSetLayoutCreateInfo,
+        compute::ComputePipelineCreateInfo,
+    },
+    memory::allocator::{
+        StandardMemoryAllocator,
+        AllocationCreateInfo,
+        MemoryTypeFilter,
+    },
+    descriptor_set::{
+        allocator::StandardDescriptorSetAllocator,
+        persistent::PersistentDescriptorSet,
+        WriteDescriptorSet,
+    },
+    command_buffer::{
+        allocator::StandardCommandBufferAllocator,
+        CommandBufferUsage,
+        AutoCommandBufferBuilder,
+    },
     instance::{
         Instance,
-        InstanceExtensions,
         InstanceCreateInfo,
+        InstanceCreateFlags,
     },
     device::{
         Device,
         DeviceCreateInfo,
-        physical::PhysicalDevice,
+        physical::PhysicalDeviceType,
         DeviceExtensions,
+        Features,
+        QueueCreateInfo,
         QueueFlags,
     },
-    swapchain::Surface,
-    Version, VulkanLibrary,
+    sync,
+    sync::GpuFuture,
+    VulkanLibrary,
 };
 
-// use std::{time, thread};
+use std::sync::Arc;
 
-struct HelloTriangleApplication {
-    event_loop: EventLoop<()>,
-    instance: Arc<Instance>,
-}
+const MINIMAL_FEATURES: Features = Features {
+    geometry_shader: true,
+    ..Features::empty()
+};
 
-impl HelloTriangleApplication {
-    pub fn new() -> Self {
+fn main() {
 
-        let library = VulkanLibrary::new()
-            .unwrap_or_else(|err| panic!("Couldn't load Vulkan library: {:?}", err));
+    // Boilerplate Initialization
+    let library = VulkanLibrary::new().unwrap();
+    let instance = Instance::new(
+        library,
+        InstanceCreateInfo {
+            flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+            ..Default::default()
+        },
+        // InstanceCreateInfo::application_from_cargo_toml(),
+    )
+    .unwrap();
 
-        let layers: Vec<_> = library.layer_properties().unwrap().collect();
+    let device_extensions = DeviceExtensions {
+        khr_storage_buffer_storage_class: true,
+        ..DeviceExtensions::empty()
+    };
 
-        let extensions = InstanceExtensions {
-            khr_surface: true,
-            khr_android_surface: false,
-            ..InstanceExtensions::empty()
-        };
+    // Getting the Device
+    let (physical_device, queue_family_index) = instance
+        .enumerate_physical_devices()
+        .unwrap()
+        .filter(|p| p.supported_extensions().contains(&device_extensions))
+        .filter_map(|p| {
+            p.queue_family_properties()
+                .iter()
+                .position(|q| q.queue_flags.intersects(QueueFlags::COMPUTE))
+                .map(|i| (p, i as u32))
+        })
+        .min_by_key(|(p, _)| match p.properties().device_type {
+            PhysicalDeviceType::DiscreteGpu => 0,
+            PhysicalDeviceType::IntegratedGpu => 1,
+            PhysicalDeviceType::VirtualGpu => 2,
+            PhysicalDeviceType::Cpu => 3,
+            PhysicalDeviceType::Other => 4,
+            _ => 5,
+        })
+        .unwrap();
 
-        let instance = Instance::new(
-            library,
-            InstanceCreateInfo {
-                enabled_extensions: extensions,
-                enabled_layers: layers.iter().map(|l| l.name().to_owned()).collect(),
+    println!(
+        "Using Device: {} (Type: {:?})",
+        physical_device.properties().device_name,
+        physical_device.properties().device_type,
+    );
+
+    if !physical_device.supported_features().contains(&MINIMAL_FEATURES) {
+        panic!("The physical device is not good enough for this application");
+    }
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            enabled_extensions: device_extensions,
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
                 ..Default::default()
+            }],
+            ..Default::default()
+        }
+    )
+    .unwrap();
+
+    let queue = queues.next().unwrap();
+
+    let pipeline = {
+        mod cs {
+            vulkano_shaders::shader! {
+                ty: "compute",
+                src: r"
+                    #version 450
+
+                    layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+
+                    layout(set = 0, binding = 0) buffer Data {
+                        uint data[];
+                    };
+
+                    void main() {
+                        uint idx = gl_GlobalInvocationID.x;
+                        data[idx] *= 12;
+                    }
+                ",
             }
-        ).unwrap_or_else(|err| panic!("Couldn't create instance: {:?}", err));
-
-        let physical = instance
-            .enumerate_physical_devices()
+        }
+        let cs = cs::load(device.clone())
             .unwrap()
-            .next()
-            .unwrap_or_else(|| panic!("Couldn't get physical device"));
-
-        let event_loop = EventLoopBuilder::new()
-            .build()
+            .entry_point("main")
             .unwrap();
 
-        let surface = WindowBuilder::new()
-            .with_title("Learning Vulkan")
-            .build(&event_loop)
-            .unwrap();
-            // .build(&event_loop, instance.clone());
-
-        let queue_family = physical
-            .queue_family_properties();
-
-        let device_ext = DeviceExtensions {
-            khr_swapchain: true,
-            ..DeviceExtensions::none()
-        };
-
-        let create_device_info = CreateDeviceInfo::new
-
-        let (device, mut queues) = Device::new(
-            physical,
-            DeviceCreateInfo::new(),
+        let stage = PipelineShaderStageCreateInfo::new(cs);
+        let layout = PipelineLayout::new(
+            device.clone(),
+            PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
+                .into_pipeline_layout_create_info(device.clone())
+                .unwrap(),
         )
         .unwrap();
 
-        Self {
-            instance,
-            event_loop,
-        }
+        ComputePipeline::new(
+            device.clone(),
+            None,
+            ComputePipelineCreateInfo::stage_layout(stage, layout),
+        ).unwrap()
+    };
+
+    let memory_allocator= Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+    let descriptor_set_allocator = Arc::new(StandardDescriptorSetAllocator::new(
+        device.clone(),
+        Default::default(),
+    ));
+    let command_buffer_allocator = Arc::new(StandardCommandBufferAllocator::new(
+        device.clone(),
+        Default::default(),
+    ));
+    let data_buffer = Buffer::from_iter(
+        memory_allocator,
+        BufferCreateInfo {
+            usage: BufferUsage::STORAGE_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        0..65536u32,
+    )
+    .unwrap();
+
+    let layout = &pipeline.layout().set_layouts()[0];
+    let set = PersistentDescriptorSet::new(
+        &descriptor_set_allocator,
+        layout.clone(),
+        [WriteDescriptorSet::buffer(0, data_buffer.clone())],
+        [],
+    )
+    .unwrap();
+
+    let mut builder = AutoCommandBufferBuilder::primary(
+        &command_buffer_allocator,
+        queue_family_index,
+        CommandBufferUsage::OneTimeSubmit,
+    )
+    .unwrap();
+
+    builder
+        .bind_pipeline_compute(pipeline.clone())
+        .unwrap()
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            pipeline.layout().clone(),
+            0,
+            set,
+        )
+        .unwrap();
+
+    builder.dispatch([1024, 1, 1]).unwrap();
+
+    let command_buffer = builder.build().unwrap();
+    let future = sync::now(device)
+        .then_execute(queue, command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+
+    future.wait(None).unwrap();
+    let data_buffer_content = data_buffer.read().unwrap();
+    for n in 0..65536u32 {
+        assert_eq!(data_buffer_content[n as usize], n * 12);
     }
 
-    fn main_loop(&mut self) {
-        let mut done = false;
-        // let interval = time::Duration::from_secs(1);
-        // thread::sleep(interval);
-        // while !done {
-        // }
-    }
-}
-
-fn main() {
-    println!("Starting Main Thing");
-    let mut app = HelloTriangleApplication::new();
-    app.main_loop();
-    println!("Finished Main Thing");
+    println!("Success!");
 }
